893907c28f83eac095faf1da74a28363111222ed
jeralxndr <jeremy.alexander4@ontariotechu.net>
2024-11-05 19:08:05 -0500

    project 2
    express install

--- index.js
+++ index.js
@@ -1,32 +1,14 @@
-// require module
-var connect = require('connect')
- 
-// create app
-var app = connect()
+const express = require('express')
+const app = express()
+const port = 3000
 
-function homePage(req,res,next)
-{
-  res.setHeader('Content-Type','text/plain');
-  res.end('Homepage');
-}
-function projectsPage(req,res,next)
-{
-  res.setHeader('Content-Type','text/plain');
-  res.end('Projects');
-}
-function aboutPage(req,res,next)
-{
-  res.setHeader('Content-Type','text/plain');
-  res.end('About');
-}
-function contactPage(req,res,next)
-{
-  res.setHeader('Content-Type','text/plain');
-  res.end('contact');
-}
-app.use('/home',homePage);
-app.use('/projects',projectsPage);
-app.use('/about',aboutPage);
-app.use('/contact',contactPage);
-app.listen(3000);
-console.log('Server is running on host')
+app.get('/helloworld', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/goodbyeworld', (req, res) => {
+  res.send('goodbye World!')
+})
+
+app.listen(port, () => {
+  console.log('Example app listening on port ${port}')
+})
