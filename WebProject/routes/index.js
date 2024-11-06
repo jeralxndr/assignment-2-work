51056130b2e3c6abd3522da6fb1f287fcf0236cf
jeralxndr <jeremy.alexander4@ontariotechu.net>
2024-11-05 20:24:05 -0500

    4

--- WebProject/routes/index.js
+++ WebProject/routes/index.js
@@ -3,7 +3,37 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'infr3120' });
+  res.render('index', { 
+    title: 'Welcome to my site!',
+   });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { 
+    title: 'Welcome to my site!',
+   });
+});
+
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { 
+    title: 'My Projects',
+   });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { 
+    title: 'About Me',
+   });
+});
+
+/* GET contacts page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { 
+    title: 'Contact Me',
+   });
 });
 
 module.exports = router;
